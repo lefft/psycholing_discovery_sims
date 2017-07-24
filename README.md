dc4f3d57fd8361273482dca9f9e75c430ceb7d42
lefft <tjleffel@gmail.com>
2017-07-24 11:05:58 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,68 @@
+### psycholinguistics discovery sims
+###### [timothy leffel](http://lefft.xyz), july24/2017
+<hr>
+
+this is a project investigating consequences of various design and analysis decisions in RT-based psycholinguistics experiments. main tool is simulation. after code infrastructure is in place, will use the literature to derive realistic effect sizes, means/sd's, etc. publicly available RT datasets also high on the wishlist. 
+
+
+inspiration comes in part from this very nice/concise/thought-provoking [note by Emmanuel Chemla](http://www.emmanuel.chemla.free.fr/thresholds/OutliersCriterion.html). 
+
+repo contents, notes to self, TODO list below. 
+<hr>
+
+<br><br><br><br><br>
+
+
+##### contents of repo:
+<hr>
+
+- `psycholing_discovery_sims.{rmd, html}` -- main document 
+- `functions.r` -- setting things up for simulating experiments + quickly displaying results (sourced at top of main doc)
+- `chemla_code.r` -- code for original chemla note (with edits/rewrite)
+- `boosh.css` -- style for main doc
+- `out/` -- plots, simulated datasets, etc. 
+
+
+<br>
+
+##### TODO list
+<hr>
+
+###### TODO items that need attention
+
+
+	> TODO: allow option for maintaining attributes of derived columns
+	> TODO: make outlier subj id's available globally (related to next two items...)
+	> TODO: GLOBAL ARG PASSING!!!
+	> TODO: need to make outlier_subj + other params globally available...
+	> TODO: implement outlier *items*, not just subj's
+	> TODO: add subj param(?)
+	> TODO: clean up passing of specify mean_rt and sd_rt!!!
+	> TODO: option to work with whole numbers(?)
+	> TODO: decide if we shd have a func for analyzing or just do it interactively..
+	> TODO: finish summary function
+	> TODO: implement rt-type labeling in plot fonce
+	> TODO: get rid of grid info in rmd output 
+	        "don't print(), grid.arrange() draws by itself. 
+	        If you want to store the result and then draw it, 
+	        use arrangeGrob()+grid.draw()"
+
+
+###### completed TODO items
+
+	-x- > TODO: add param for number of outlier subj's
+	-x- > TODO: finish plotting function
+	-x- > TODO: write func to extract relevant bits from model summaries
+
+<br>
+
+##### misc notes to self
+<hr>
+
+	> 
+	> 
+	> ...
+
+
+
+
---
+++ functions.r
@@ -0,0 +1,208 @@
+require("dplyr"); require("magrittr"); require("ggplot2"); require("gridExtra")
+
+# sim funcs etc.
+
+# calls generate data and introduce outliers
+# simulate_experiment <- function(n_subj, n_item, n_cond, derived_vars=TRUE){
+simulate_experiment <- function(){
+  dat <- generate_data()
+  dat <- introduce_outliers(dat=dat)
+  dat <- transform_rts(dat=dat)
+  return(dat)
+}
+
+
+# NOT WORKING AS-IS -- NEEDS CHAR CONVERSION + IMPLEMENT RT CHOICE
+# plot the data w a specific col for reaction time (e.g. log/scaled/etc.)
+plot_results <- function(dat, rt_col=NULL){
+  
+  col_dict <- setNames(c("raw RT","log RT","RT, centered","RT, z-scored"), 
+                       nm=c("rt", "rt_log", "rt_ctd", "rt_zsc"))
+  
+  # description of the sim setup
+  descript <- attributes(dat)$comment
+  subj_info <- attributes(dat)$bot
+  
+  # plot the data to make sure it looks as desired
+  rt_by_cond <- ggplot(
+    dat, aes_string(x=rt_col, fill="cond", color="cond")
+    ) + 
+    geom_density(alpha=.1) + # or: # geom_histogram(bins=20, position="dodge")
+    labs(title=paste0("1. measure: ", as.character(col_dict[rt_col])), 
+         subtitle="by condition") + 
+    theme(legend.position="bottom", axis.text.y=element_blank()) + 
+    scale_fill_manual(values=c("#dbc13f","#176614")) + 
+    scale_color_manual(values=c("#dbc13f","#176614")) # yellow, green
+  
+  rt_by_cond_outlier <- ggplot(
+    dat, aes_string(x=rt_col, fill="cond", color="cond")
+    ) + 
+    geom_density(alpha=.1) + facet_wrap(~is_outlier) + 
+    theme(legend.position="none", axis.text.y=element_blank()) + 
+    labs(title=paste0("3. measure: ", as.character(col_dict[rt_col])), 
+         subtitle="by outlier and condition") + 
+    scale_fill_manual(values=c("#dbc13f","#176614")) + 
+    scale_color_manual(values=c("#dbc13f","#176614")) # yellow, green
+  
+  rt_by_outlier <- ggplot(
+    dat, aes_string(x=rt_col, fill="is_outlier", color="is_outlier")
+    ) + 
+    geom_density(alpha=.1) + 
+    theme(legend.position="bottom", axis.text.y=element_blank()) + 
+    labs(title=paste0("2. measure: ", as.character(col_dict[rt_col])), 
+         subtitle="by outlier status") + 
+    scale_fill_manual(values=c("#ba671f","blue")) + 
+    scale_color_manual(values=c("#ba671f","blue"))
+  
+  rt_by_outlier_cond <- ggplot(
+    dat, aes_string(x=rt_col, fill="is_outlier", color="is_outlier")
+    ) + 
+    geom_density(alpha=.1) + facet_wrap(~cond) + 
+    theme(legend.position="none", axis.text.y=element_blank()) + 
+    labs(title=paste0("4. measure: ", as.character(col_dict[rt_col])), 
+         subtitle="by condition and outlier") + 
+    scale_fill_manual(values=c("#ba671f","#1264ce")) + 
+    scale_color_manual(values=c("#ba671f","#1264ce")) # orange, blue
+  
+  out <- grid.arrange(
+    # the grobs
+    rt_by_cond, rt_by_cond_outlier, rt_by_outlier, rt_by_outlier_cond, 
+    # their relative position/layout
+    ncol=2, layout_matrix = cbind(c(1,2), c(3,4)), 
+    top=paste0(descript,"\n"), bottom=subj_info
+  )
+  
+  return(out)
+}
+
+
+# NOT WORKING AS-IS -- NEEDS CHAR CONVERSION + IMPLEMENT RT CHOICE
+# plot the data w a specific col for reaction time (e.g. log/scaled/etc.)
+summarize_results <- function(dat, rt_col=NULL){
+  
+  # summary tables
+  
+  # just grouping by condition
+  dat %>% group_by(cond) %>% summarize(
+    num_obs = n(), 
+    rt_mean = mean(rt),
+    rt_sd   = sd(rt)
+  ) %>% knitr::kable(digits=0, caption=comment(dat)) -> summary_by_cond
+  
+  # just grouping by outlier status
+  dat %>% group_by(is_outlier) %>% summarize(
+    num_obs = n(), 
+    rt_mean = mean(rt),
+    rt_sd   = sd(rt)
+  ) %>% knitr::kable(digits=0, caption=comment(dat)) -> summary_by_outlier
+  
+  # group by condition and by subj
+  dat %>% group_by(is_outlier, cond) %>% summarize(
+    num_obs = n(), 
+    rt_mean = mean(rt),
+    rt_sd   = sd(rt)
+  ) %>% knitr::kable(digits=0, caption=comment(dat)) -> summary_by_outlier_cond
+  
+  # return all three as a list
+  return(list(summary_by_cond=summary_by_cond, 
+              summary_by_outlier=summary_by_outlier, 
+              summary_by_outlier_cond=summary_by_outlier_cond))
+}
+
+
+
+# function to simulate some reasonable data
+generate_data <- function(n_subj=100, n_item=20, n_cond=2, 
+                          mean_rt=500, sd_rt=100, effect=100){
+  # get the params
+  # n_subj <- 100
+  # n_item <- 20
+  # n_cond <- 2
+  
+  # set up most of the data, other than rt's
+  dat <- data.frame(
+    subj = rep(sprintf("subj_%03d", seq(len=n_subj)), each=n_item),
+    item = rep(sprintf("item_%02d", seq(len=n_item)), times=n_subj),
+    cond = rep(c("related", "_unrelated"), each=n_item/n_cond, times=n_subj),
+    rt   = numeric(n_subj*(n_item/n_cond)), 
+    stringsAsFactors=FALSE
+  )
+  
+  # params for the response data
+  # mean_rt <- 500
+  # sd_rt <- 100
+  # effect <- 100
+  
+  # randomly generate rt's for each condition
+  rt_unrelated <- rnorm(n_subj*(n_item/n_cond), mean=mean_rt, sd=sd_rt) 
+  rt_related   <- rnorm(n_subj*(n_item/n_cond), mean=mean_rt-effect, sd=sd_rt) 
+  
+  # now add the response data to the df
+  dat$rt[dat$cond=="related"] <- rt_related
+  dat$rt[dat$cond=="_unrelated"] <- rt_unrelated
+  
+  # attach metadata for plotting/modeling in the future
+  comment(dat) <- paste0(
+    "mean RT: ", mean_rt, ";  ",
+    "sd of RT: ", sd_rt, ";  ",
+    "effect: ", effect, "ms"
+  )
+  attr(dat, "bot") <- paste0(
+    "num subj: ", n_subj, ";  ", "num item: ", n_item, 
+    " (", n_subj*n_item, " data points)"
+  )
+  
+  # return the simulated data
+  return(dat)
+}
+
+
+# this will cause some of the subj to have a different effect
+introduce_outliers <- function(dat=dat, n_outlier=10, outlier_effect=-100){
+  
+  # now replace the random rt's with weird rt's for outlier subj's
+  # n_outlier <- 10
+  # outlier_effect <- -100
+  
+  # for development, have the outliers be 01-10
+  # outlier_subj <- sample(dat$subj, size=n_outlier, replace=FALSE)
+  outlier_subj <- sort(unique(as.character(dat$subj)))[seq(len=n_outlier)]
+  
+  # gotta get number of items etc [THIS IS SLOPPY]
+  n_item <- length(unique(dat$item))
+  n_cond <- length(unique(dat$cond))
+  mean_rt <- 500
+  sd_rt <- 100
+  
+  
+  rt_related_outlier <- 
+    rnorm(n_outlier*(n_item/n_cond), mean=mean_rt-outlier_effect, sd=sd_rt)
+  
+  dat$rt[dat$subj %in% outlier_subj & dat$cond=="related"] <- rt_related_outlier
+  
+  # record who the outliers are for plotting
+  dat$is_outlier <- ifelse(dat$subj %in% outlier_subj, "_outlier", "reggie")
+  
+  # add info about the outliers
+  comment(dat) <- paste0(
+    comment(dat), "\n", 
+    "num outlier subj: ", n_outlier, ";  ", 
+    "effect for outliers: ", outlier_effect, "ms"
+  )
+  
+  # return the df
+  return(dat)
+}
+
+
+# function to do some common rt transformations
+transform_rts <- function(dat=dat){
+  # make a few derived transformations of the rt's for analysis
+  dat$rt_log <- as.numeric(log(dat$rt))
+  dat$rt_ctd <- as.numeric(scale(dat$rt, center=TRUE, scale=FALSE)) # zero mean
+  dat$rt_zsc <- as.numeric(scale(dat$rt, center=TRUE, scale=TRUE))  # z-score
+  
+  # and return the data
+  return(dat)
+}
+
